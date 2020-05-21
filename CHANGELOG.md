4493bdfb30c9fad3b668670547a98e32b771426f
Guilherme Andrade <guilherme.andrade@miniclip.com>
2020-05-21 11:11:31 +0100

    Add compatibility with OTP 23
    Make TLS connections backed by a wildcard certificate work.
    
    They otherwise wouldn't, due to an OTP bugfix[*].
    
    [*]: https://bugs.erlang.org/browse/ERL-1232

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # CHANGELOG
 
+## [Unreleased]
+
+* Added:
+    * compatibility with OTP 23
+
 ## v2.2.0
 
 * Changed:
--- src/eredis_sub_client.erl
+++ src/eredis_sub_client.erl
@@ -341,7 +341,7 @@ transport_params(State) when State#state.transport =:= tcp ->
      tcp, tcp_closed, tcp_error};
 transport_params(State) when State#state.transport =:= ssl ->
     Host = State#state.host,
-    ConnectOpts = eredis_security:secure_ssl_opts(Host, ?SSL_SOCKET_OPTS),
+    ConnectOpts = ?SSL_SOCKET_OPTS ++ tls_certificate_check:options(Host),
     {ssl, ConnectOpts,
      ssl, ssl_closed, ssl_error}.
 
